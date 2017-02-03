d75f4c28424ee10875d8a17ce7b9607fbcf7892c
John10 Korsnes <johnkors@gmail.com>
2017-02-03 09:31:14 +0100

    Minimal MVC proj

---
+++ Idsrv.Discourse/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Idsrv.Discourse.MvcApplication" Language="C#" %>
---
+++ Idsrv.Discourse/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Idsrv.Discourse.Startup))]
+
+namespace Idsrv.Discourse
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            
+        }
+    }
+}

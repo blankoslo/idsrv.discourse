91d72a8dc6a5f3217ce1ceb295389e64cf217c21
Lars Smeby <lars@smebyfx.no>
2017-02-03 09:58:29 +0100

    Updated DiscourseController

--- Idsrv.Discourse/Controllers/DiscourseController.cs
+++ Idsrv.Discourse/Controllers/DiscourseController.cs
@@ -12,64 +12,147 @@ namespace Idsrv.Discourse.Controllers
 {
     public class DiscourseController : Controller
     {
-
+        // Redirected from Discourse
         [Route("core/discourse")]
-        public ActionResult Index(string username, string password)
+        public ActionResult Index([Bind(Prefix = "sso")]string payload, [Bind(Prefix = "sig")]string signature)
         {
-            var res = Request.GetOwinContext().Environment.GetIdentityServerFullLoginAsync().GetAwaiter().GetResult();
-            if (res != null && res.Claims.Any())
+            if (string.IsNullOrEmpty(payload) || string.IsNullOrEmpty(signature))
             {
-                var user = Users.Get().First(u => u.Username == res.Name.ToString());
-                var discourseResponse = CreateCustomDiscourseResponse(user);
-                return
-                    new RedirectResult("https://localhost:44319/identity/discourse/mock?alreadyauth=1&payload=" +
-                                       discourseResponse);
+                throw new ArgumentException("Missing input parameters");
             }
-            return View(new {username, password});
+
+            if (!ValidatePayload(payload, signature))
+            {
+                throw new SecurityException("Payload signature not valid");
+            }
+
+            var res = Request.GetOwinContext().Environment.GetIdentityServerFullLoginAsync().GetAwaiter().GetResult();
+
+            if (res == null || !res.Claims.Any())
+            {
+                // Not authenticated, returning login page
+                TempData["payload"] = payload;
+                return View();
+            }
+
+            // User authenticated, getting user from db, generating payload and redirecting back to Discourse
+            var user = _userService.GetUser(res.Name);
+
+            if (user == null)
+            {
+                throw new UserNotFoundException($"Could not get user with username '{res.Name}' from db");
+            }
+
+            var redirectUrl = CreateDiscourseRedirectUrl(user, payload);
+            return new RedirectResult(redirectUrl);
         }
 
         [Route("core/discourse/login")]
         [HttpPost]
-        public ActionResult Login(string username, string password)
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(Login login)
         {
-            var user = Users.Get().FirstOrDefault(u => u.Username == username && u.Password == password);
-            if (user != null)
+            if (_authenticationService.Authenticate(login.Email, login.Password))
             {
+                var user = _userService.GetUser(login.Email);
+                if (user == null)
+                {
+                    throw new UserNotFoundException($"Could not get user with username '{login.Email}' from db");
+                }
+
                 var authLogin = new AuthenticatedLogin
                 {
                     AuthenticationMethod = "password",
-                    Name = username,
-                    Claims = new List<Claim>
-                    {
-                        new Claim(Constants.ClaimTypes.PreferredUserName, username),
-                        new Claim(Constants.ClaimTypes.GivenName,
-                            user.Claims.First(c => c.Type == Constants.ClaimTypes.GivenName).Value),
-                        new Claim(Constants.ClaimTypes.FamilyName,
-                            user.Claims.First(c => c.Type == Constants.ClaimTypes.FamilyName).Value)
-                    },
-                    Subject = user.Subject,
+                    Name = user.Email,
+                    Subject = user.Email,
                     PersistentLogin = true
                 };
+
                 Request.GetOwinContext().Environment.IssueLoginCookie(authLogin);
-                var discourseResponse = CreateCustomDiscourseResponse(user);
-                return new RedirectResult("https://localhost:44319/identity/discourse/mock?payload=" + discourseResponse);
+
+                var payload = TempData["payload"] as string;
+                if (payload == null)
+                {
+                    throw new Exception("Unable to retrieve Discourse payload from memory");
+                }
+
+                TempData["payload"] = null;
+
+                var redirectUrl = CreateDiscourseRedirectUrl(user, payload);
+                return new RedirectResult(redirectUrl);
             }
-            else
+
+            TempData["error"] = "Wrong username or password";
+            return View("Index");
+        }
+
+        [Route("identity/discourse/logout")]
+        public ActionResult Logout()
+        {
+            Request.GetOwinContext().Authentication.SignOut();
+            return Redirect(AppSettings.DiscoursePostLogoutRedirectUri());
+        }
+
+
+
+
+        public static bool ValidatePayload(string encodedPayload, string signature)
+        {
+            return Hash(AppSettings.DiscourseSecret(), encodedPayload) == signature;
+        }
+
+        public static string CreateDiscourseRedirectUrl(User user, string originalEncodedPayload)
+        {
+            var urlParameters = ParsePayload(originalEncodedPayload);
+            var nonce = urlParameters.Get("nonce");
+            var returnUrl = urlParameters.Get("return_sso_url");
+            
+            var payloadDictionary = new Dictionary<string, string>
             {
-                TempData["error"] = "Wrong username + password";
-                return RedirectToAction("Index");
+                {"nonce", nonce},
+                {"email", HttpUtility.UrlEncode(user.Email)},
+                {"external_id", HttpUtility.UrlEncode(user.Id.ToString())},
+                {"username", HttpUtility.UrlEncode(user.Email)},
+                {"name", HttpUtility.UrlEncode(user.Name)},
+                {"avatar_url", HttpUtility.UrlEncode(user.ImageUrl)},
+                {"bio", HttpUtility.UrlEncode(user.Description)},
+                {"suppress_welcome_message", "true"}
+            };
+
+            if (user.CompanyUser)
+            {
+                payloadDictionary.Add("moderator", "true");
             }
+            if (user.Administrator)
+            {
+                payloadDictionary.Add("admin", "true");
+            }
+
+            var returnPayload = CreatePayloadQueryString(payloadDictionary);
+
+            var returnPayloadEncoded = Convert.ToBase64String(Encoding.UTF8.GetBytes(returnPayload));
+            var returnSig = Hash(AppSettings.DiscourseSecret(), returnPayloadEncoded);
+
+            return $"{returnUrl}?sso={returnPayloadEncoded}&sig={returnSig}";
         }
 
-        [Route("core/discourse/mock")]
-        public ContentResult Mock(string payload, string alreadyauth)
+        private static string Hash(string key, string payload)
         {
-            return new ContentResult {Content = payload + " - AlreadyAuthenticated?:" + alreadyauth};
+            var hasher = new HMACSHA256(Encoding.UTF8.GetBytes(key));
+            var hash = hasher.ComputeHash(Encoding.UTF8.GetBytes(payload));
+            return BitConverter.ToString(hash).Replace("-", "").ToLowerInvariant();
         }
 
-        private string CreateCustomDiscourseResponse(InMemoryUser user)
+        private static NameValueCollection ParsePayload(string encodedPayload)
         {
-            return string.Format("{0} : {1} ", user.Username, user.Subject);
+            var queryString = Encoding.UTF8.GetString(Convert.FromBase64String(encodedPayload));
+            return HttpUtility.ParseQueryString(queryString);
+        }
+
+        private static string CreatePayloadQueryString(Dictionary<string, string> dictionary)
+        {
+
+            return string.Join("&", dictionary.Select(x => $"{x.Key}={x.Value}"));
         }
     }
 }
